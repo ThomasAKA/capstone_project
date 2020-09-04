0748c02a76511781cdddeeaa158e4076a46686a8
Priscilla-Odame <42655518+Priscilla-Odame@users.noreply.github.com>
2020-09-04 11:16:24 +0100

    Edit serializer_class

--- eventapp/backend/usermanagement/views.py
+++ eventapp/backend/usermanagement/views.py
@@ -11,7 +11,7 @@ from knox.models import AuthToken
 # Create your views here.
 
 class UserView(viewsets.ModelViewSet):
-    serialiizer_class = UserSerializer
+    serializer_class = UserSerializer
     queryset = User.objects.all()
 
 

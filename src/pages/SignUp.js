ea23a56028e54a64eaf09dfcd02b8733297f3a04
Anna Asunka <anna.asunka@linqworth.com>
2020-09-04 01:50:23 +0000

    api not responding

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -6,6 +6,7 @@ import {
     faTwitter,
     faInstagram
   } from "@fortawesome/free-brands-svg-icons";
+import axios from 'axios';
 class SignUp extends Component {
     constructor() {
         super();
@@ -13,15 +14,17 @@ class SignUp extends Component {
         this.state = {
             email: '',
             password: '',
-            firstName: '',
-            lastName: '',
-            confirmPassword: '',
-            hasAgreed: false
+            username: '',
+            // firstName: '',
+            // lastName: '',
+            // confirmPassword: '',
+            hasAgreed: true
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
 
 
     handleChange(e) {
@@ -36,31 +39,50 @@ class SignUp extends Component {
     }
 
     handleSubmit(e) {
-        e.preventDefault();
+        e.preventDefault()
         console.log('The form was submitted with the following data:');
         console.log(this.state);
-    }
+    
+  }
+    componentDidMount(){
+    axios.post("http://127.0.0.1:8000/api/register/")
+    .then(response =>{
+      console.log(response);
+      if(response.status === 200){
+        console.log("SignUp successful");
+      }
+      else{
+        console.log(response)
+      };
+    })
+    .catch(error => {
+      console.log(error);
+      
+    });
+  }
+  
+
 
     render() {
         return (
         <div className="FormCenter">
             <form onSubmit={this.handleSubmit} className="FormFields">
               <div className="FormField">
-                <label className="FormField__Label" htmlFor="name">First Name</label> 
-                <input type="text" id="name" className="FormField__Input" placeholder="Enter your first name" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
+                <label className="FormField__Label" htmlFor="name">username</label> 
+                <input type="text" id="name" className="FormField__Input" placeholder="Enter your first name" name="username" value={this.state.username} onChange={this.handleChange} />
               </div>
-              <div className="FormField">
+              {/* <div className="FormField">
                 <label className="FormField__Label" htmlFor="lastName">Last Name</label>
                 <input type="text" id="lastName" className="FormField__Input" placeholder="Enter your last name" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
-              </div>
+              </div> */}
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="password">Password</label>
                 <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
               </div>
-              <div className="FormField">
+              {/* <div className="FormField">
                 <label className="FormField__Label" htmlFor="confirmPassword">Confirm Password</label>
                 <input type="password" id="confirmPassword" className="FormField__Input" placeholder="Confirm your password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChange} />
-              </div>
+              </div> */}
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
                 <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />

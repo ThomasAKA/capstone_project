6980be23d44e6e1898438222f1711c0884cfb082
Jonathan Nsiah <jooankrah@gmail.com>
2020-09-07 10:41:55 +0000

    modified:   .gitignore 	new file:   build/asset-manifest.json 	new file:   build/favicon.ico 	new file:   build/index.html 	new file:   build/logo192.png 	new file:   build/logo512.png 	new file:   build/manifest.json 	new file:   build/precache-manifest.36ce9a41b5c891afdf684253c839bf27.js 	new file:   build/robots.txt 	new file:   build/service-worker.js 	new file:   build/static/css/main.d9f98ec9.chunk.css 	new file:   build/static/css/main.d9f98ec9.chunk.css.map 	new file:   build/static/js/2.f54fd2b2.chunk.js 	new file:   build/static/js/2.f54fd2b2.chunk.js.LICENSE.txt 	new file:   build/static/js/2.f54fd2b2.chunk.js.map 	new file:   build/static/js/main.61d1a526.chunk.js 	new file:   build/static/js/main.61d1a526.chunk.js.map 	new file:   build/static/js/runtime-main.f222f6be.js 	new file:   build/static/js/runtime-main.f222f6be.js.map

---
+++ build/precache-manifest.36ce9a41b5c891afdf684253c839bf27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358956db5aefd1f8d2cdb130ae10185c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "732a84f87abb7933350d",
+    "url": "/static/css/main.d9f98ec9.chunk.css"
+  },
+  {
+    "revision": "56e0f4ef79945f51d894",
+    "url": "/static/js/2.f54fd2b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f54fd2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732a84f87abb7933350d",
+    "url": "/static/js/main.61d1a526.chunk.js"
+  },
+  {
+    "revision": "37f81c5678d064cc374e",
+    "url": "/static/js/runtime-main.f222f6be.js"
+  }
+]);

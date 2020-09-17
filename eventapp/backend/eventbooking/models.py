94924e58cd3a1e857da9b44d8067587da7a9fba8
Prisccilla-Odame <priscillaodame@gmail.com>
2020-09-17 16:38:17 +0000

    Make time field in eventbooking unique

--- eventapp/backend/eventbooking/models.py
+++ eventapp/backend/eventbooking/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from usermanagement.models import UserAccess
+from usermanagement.models import UserAccess,User
 from event.models import Event
 
 # Create your models here.
 
 class Booking(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
-    user = models.ForeignKey(
-        UserAccess, on_delete=models.CASCADE, null=True, related_name="booked")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
  
     times = (
         ('Morning', 'Morning'),
@@ -16,7 +15,7 @@ class Booking(models.Model):
     )
  
     time = models.CharField(verbose_name='time', max_length=50,
-           choices=times, default='')
+           choices=times, default='', unique=True)
 
 
 

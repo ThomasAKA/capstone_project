a7683501b70e1eaffdff5229e140c3dff1609fa1
Jonathan Nsiah <jooankrah@gmail.com>
2020-09-25 11:09:01 +0000

    modified:   src/axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 const token = localStorage.getItem('token');
 
 export  const axiosinstance = axios.create({
-    baseURL: 'http://13.92.41.24//api/',
+    baseURL: 'http://13.92.41.24/api/',
   //  headers: {'Authorization' : `Token ${token}`}
   });
